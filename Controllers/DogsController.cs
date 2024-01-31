6b4e4d3ed1dbdb41c3b26b002f8c8e918e56a898
hdfredl <dat.leef@gmail.com>
2024-01-31 10:04:37 +0100

    Add dogs controller

---
+++ Controllers/DogsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CutestDogsAdoptionService.Controllers;
+
+public class DogsController : Controller
+{
+
+    public IActionResult Index()
+    {
+
+        return View();
+
+    }
+
+}
